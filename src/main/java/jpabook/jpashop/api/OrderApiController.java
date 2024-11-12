d84e709fa5c92ec71eec1bf78f5bb126c03805b2
ChanUi <hcu55@naver.com>
2024-11-12 12:39:16 +0900

    :sparkles: 주문 조회 V4: JPA에서 DTO 직접 조회

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -73,6 +73,11 @@ public class OrderApiController {
         return result;
     }
 
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> ordersV4() {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
     @Getter
     static class OrderDto {
 
